6080c6af6d0cfb5f7d997d101cf53d27e5c48282
euntaek <108321588+euntaek4187@users.noreply.github.com>
2025-04-11 09:17:23 +0900

    Prev (#3)
    * feat: 기존 파일 업로드
    
    * Create README.md
    
    * Update README.md
    
    * [Feat] SoldOut check, Create menu #1
    
    * [Build] Add yml && build #1
    
    * [Feat] Implement ticket fetch in frontend #1
    
    * feat: add Review, lookup congestion
    
     - 리뷰 1개/여러 개 추가 api
     - 조식/중식/석식 혼잡도 조회 api
    
    * refactor: CongestionService
    
     - 조건문 부분 수정, 코드 중복 줄임
    
    * [Fix] getTimeSlots DateTimeFormatter H:mm to HH:mm
    
    * [Fix] API Routing
    
    * feat: 기존 파일 업로드
    
    * Update Dockerfile
    
    * Update AddReviewController.java
    
    ---------
    
    Co-authored-by: yeilkk <yeil99@naver.com>
    Co-authored-by: BEOMJIN754 <161280497+BEOMJIN754@users.noreply.github.com>
    Co-authored-by: dlqja <beomjin754@gmail.com>
    Co-authored-by: user122938 <69978045+user122938@users.noreply.github.com>

--- src/main/java/com/example/teamproject/controller/AddReviewController.java
+++ src/main/java/com/example/teamproject/controller/AddReviewController.java
@@ -1,5 +1,4 @@
 package com.example.teamproject.controller;
-
 import com.example.teamproject.domain.review.dto.ReviewRequestDto;
 import com.example.teamproject.domain.review.entity.Review;
 import com.example.teamproject.domain.review.service.AddReviewService;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
